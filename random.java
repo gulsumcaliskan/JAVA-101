8239e5627688a5e6f1bff092af675d4229826400
gulsumcaliskan <gulsumfb95@gmail.com>
2022-01-19 14:57:33 +0300

    Add files via upload
    Not-Hesaplama

---
+++ random.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class random {
+    public static void main(String[] args) {
+        
+        int mat, fizik, kimya, turkce, tarih, muzik;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Matematik notunuz: ");
+        mat = input.nextInt();
+        
+        System.out.print("Fizik notunuz: ");
+        fizik = input.nextInt();
+        
+        System.out.print("Kimya notunuz: ");
+        kimya = input.nextInt();
+        
+        System.out.print("Turkce notunuz: ");
+        turkce = input.nextInt();
+        
+        System.out.print("Tarih notunuz: ");
+        tarih = input.nextInt();
+        
+        System.out.print("Muzik notunuz: ");
+        muzik = input.nextInt();
+        
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double ort = (toplam / 6.0);   
+        
+        boolean notSonuc = (ort >= 60);
+        
+        String sonuc = (notSonuc) ? "Gecti" : "Kaldi";
+        
+        System.out.print(ort + " ortalamasý ile " + sonuc);
+    }
+    
+}
