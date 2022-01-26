90279453c96e43ef548fcef1bb2bd54441837cf2
gulsumcaliskan <gulsumfb95@gmail.com>
2022-01-26 15:48:13 +0300

    Ters Üçgen Yapımı

---
+++ tersucgen.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class tersucgen {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Basamak sayýsýný giriniz: ");
+		int n = input.nextInt();
+		
+		for (int i = n - 1; i > 0; i--){
+			for (int j = n; j > i; j--){
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= (2*(i - 1)) + 1; k++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
