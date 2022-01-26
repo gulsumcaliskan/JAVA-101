637dd94ff255db3abc10fa62bd66a787d098f742
gulsumcaliskan <gulsumfb95@gmail.com>
2022-01-26 15:55:24 +0300

    1-100 Arasındaki Asal Sayıları Bulan Program

---
+++ asal.java
@@ -0,0 +1,24 @@
+
+public class asal {
+	public static void main(String[] args) {
+		
+		int sayac = 0;
+		
+		for (int i = 2; i <= 100; i++){
+			int control = 0;
+			for (int j = 2; j < i; j++){
+				if (i % j == 0){
+					control = 1;
+					break;
+				}
+			}
+			if (control == 0){
+				System.out.print(i + " ");
+				sayac++;
+			}
+		}
+		
+		
+	}
+
+}
