58578da8ff5c6a2c367d42db2164d7d17af8df5e
gulsumcaliskan <gulsumfb95@gmail.com>
2022-01-31 18:29:13 +0300

    Dizi_ Harmonik Ortalama

---
+++ harmonikOrt.java
@@ -0,0 +1,18 @@
+
+public class harmonikOrt {
+	public static void main(String[] args){
+
+		
+		int[] numbers = {1, 2};
+		double sum = 0;
+		
+		for(int i = 0; i < numbers.length; i++){
+			sum += 1.0/numbers[i];
+		}
+		
+		
+		System.out.println(numbers.length / sum);
+	
+	}
+
+}
