2e82d4cf461a3ba0a41144f23eb24d067758d47c
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-04-25 23:42:50 +0300

    Note Average

--- NoteAverage.java
+++ NoteAverage.java
@@ -10,23 +10,23 @@ public class NoteAverage {
 		
 		System.out.print("Please enter your math note: ");
 		math = input.nextInt();
-		int math = (math >= 0 && math < 100) ? math : 0;
+		math = (math >= 0 && math < 100) ? math : 0;
 		
-		System.out.print("Fizik notunu giriniz: ");
+		System.out.print("Please enter your physic note: ");
 		physic = input.nextInt();
-		int physic = (physic >= 0 && physic < 100) ? physic : 0;
+		physic = (physic >= 0 && physic < 100) ? physic : 0;
 		
-		System.out.print("Türkçe notunu giriniz: ");
+		System.out.print("Please enter your turkish note: ");
 		turkish = input.nextInt();
-		int turkish = (turkish >= 0 && turkish < 100) ? turkish : 0;
+		turkish = (turkish >= 0 && turkish < 100) ? turkish : 0;
 		
-		System.out.print("Kimya notunu giriniz: ");
+		System.out.print("Please enter your chemistry note: ");
 		chemistry = input.nextInt();
-		int chemistry = (chemistry >= 0 && chemistry < 100) ? chemistry : 0;
+		chemistry = (chemistry >= 0 && chemistry < 100) ? chemistry : 0;
 		
-		System.out.print("Müzik notunu giriniz: ");
+		System.out.print("Please enter your music note: ");
 		music = input.nextInt();
-		int music = (music >= 0 && music < 100) ? music : 0;
+		music = (music >= 0 && music < 100) ? music : 0;
 		
 		double average = ((math + physic + turkish + chemistry + music) / 5);
 		if (average <= 55) {
