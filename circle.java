f41709e23a3e1b721d7d2afb3532c704526dcb7d
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-04-27 00:30:20 +0300

    Circle Area

--- circle.java
+++ circle.java
@@ -1,21 +1,22 @@
 package daire;
 
 import java.util.Scanner;
-public class circle {
-	public static void main(String[] args) {
-		
-		double r, alan, cevre, a;
+
+public class CircleArea {
+    public static void main(String args[]) {
+      double r, area, perimeter, a;
 		Scanner input = new Scanner(System.in);
-		System.out.print("Yarýçapý giriniz: ");
+		System.out.print("Enter a radius (r) value: ");
 		r = input.nextDouble();
-		System.out.print("Merkez açýsý ölçüsünü giriniz: ");
+		System.out.print("Enter a central angle (a) value: ");
 		a = input.nextDouble();
 		
-		alan = (3.14 * r * r);
-		cevre = (2 * 3.14 * r);
+		area = (3.14 * r * r);
+		perimeter = (2 * 3.14 * r);
 		a = ((3.14 * (r * r) * a) / 360);
-		System.out.println("Alan: " + alan + " çevre: " + cevre);
-		System.out.println("Merkez açýsýnýn ölçüsü: " + a);
-		
-	}
+		System.out.println("-------------------");
+		System.out.println("Area: " + area);
+		System.out.println("Perimeter: " + perimeter);
+		System.out.println("Central angle: " + a);
+    }
 }
