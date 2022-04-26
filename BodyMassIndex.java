2a352a5fc180890df644c70033f6cb8a01dadd6a
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-04-27 00:44:28 +0300

    Body Mass Index

---
+++ BodyMassIndex.java
@@ -0,0 +1,19 @@
+package kitle;
+
+import java.util.Scanner;
+public class BodyMassIndex {
+	public static void main(String[] args) {
+		
+		double height, weight, bodyMass;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your value of height (in terms of m): ");
+		height = input.nextDouble();
+		System.out.print("Enter your value of weight: ");
+		weight = input.nextDouble();
+		bodyMass = (weight / (height * height));
+		
+		System.out.println();
+		System.out.print("Your Body Mass Index is: " + bodyMass);
+	}
+}
