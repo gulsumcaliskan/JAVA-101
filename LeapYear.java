3bb9f6363d0ad07d5438f44d30b6945bf8c2b295
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-04-27 01:31:57 +0300

    Leap Year Calculator

--- LeapYear.java
+++ LeapYear.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
-public class artik {
+
+public class LeapYear {
 	public static void main(String[] args){
 		
 		int year; 
-		boolean artik = false;
+		boolean leapYear = false;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Yýl giriniz: ");
+		System.out.print("Enter the year: ");
 		year = input.nextInt();
 		
 		
 		if (year % 4 == 00){
 			if (year % 100 == 0){
 				if (year % 400 ==0){
-					artik = true;
+					leapYear = true;
 				}else {
-					artik = false;
+					leapYear = false;
 				}
 			}else {
-				artik = true;
+				leapYear = true;
 			}
 		}else {
-			artik = false;
+			leapYear = false;
 		}
-		if(artik) {
-			System.out.println(year + " bir artýk yýdýr.");
+		if(leapYear) {
+			System.out.println(year + " is a leap year.");
 		}else {
-			System.out.println(year + " bir artýk yýl deðildir.");
+			System.out.println(year + " is not a leap year.");
 		}
 	}
 
