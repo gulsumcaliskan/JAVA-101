3e0fb96118c2d4d5e5950099e5251d93a9d9c905
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-04-27 22:30:22 +0300

    Greatest Common Divisor - Least Common Divisor

--- GCDLCM.java
+++ GCDLCM.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
-public class ebob {
+
+public class GCDLCM {
 	public static void main(String[] args){
 		
 		Scanner input = new Scanner (System.in);
 		
-		System.out.print("n1 sayýsýný giriniz: ");
+		System.out.print("Enter the number of n1: ");
 		int n1 = input.nextInt();
 		
-		System.out.print("n2 sayýsýný giriniz: ");
+		System.out.print("Enter the number of n2: ");
 		int n2 = input.nextInt();
 		
-		int ebob = 1;
-		int ekok = (n1 > n2) ? n1: n2;
+		int gcd = 1;
+		int lcd = (n1 > n2) ? n1: n2;
 		
 		while(true){
-			if (ekok % n1 == 0 && ekok % n2 == 0){
-				System.out.println(n1 + " ve " + n2 + " sayýlarýnýn EKOK deðeri: " + ekok);
+			if (lcd % n1 == 0 && lcd % n2 == 0){
+				System.out.println(n1 + " and " + n2 + " Greatest common divisor: " + lcd);
 				break;
 			}
-			++ekok;
+			++lcd;
 		}
 			
 		System.out.println("**********************");
 		
 		for (int k = n1; k >= 1; k--){
 			if (n1 % k == 0 && n2 % k == 0){
-				ebob = k;
-				System.out.println(ebob);
+				gcd = k;
+				System.out.println(gcd);
 				break;
 			}
 		}	
