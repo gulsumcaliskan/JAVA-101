4acd8965b46c23625ea65eb54492f2ce49a88c40
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-04-27 22:22:10 +0300

    Star Triangle

--- StarTriangle.java
+++ StarTriangle.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-public class yildiz {
+
+public class StarTriangle {
 	public static void main(String[] args){
 		
 		Scanner input = new Scanner(System.in);
-		System.out.print("Basamak sayýsýný giriniz: ");
+		System.out.print("Enter number of steps: ");
 		int n = input.nextInt();
 		
 		for(int i = 1; i <= n; i++){
