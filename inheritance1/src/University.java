1d9a83414159a5fd3a7a8dd97c9210772844a5d3
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-05-13 16:19:13 +0300

    University

--- inheritance1/src/University.java
+++ inheritance1/src/University.java
@@ -2,31 +2,31 @@
 public class University {
 	public static void main(String[] args){
 		
-		Calisan c1 = new Calisan("Althea Yielding", "1141204231", "ayielding0@domainmarket.com");
-//		c1.giris();
+		Personnel p1 = new Personnel("Althea Yielding", "1141204231", "ayielding0@domainmarket.com");
+//		p1.enter();
 		
-		Akademisyen a1 = new Akademisyen("Leonie Blues", "83249145133", "lblues1@ucoz.com", "CENG", "Doçent");
+		Academician a1 = new Academician("Leonie Blues", "83249145133", "lblues1@ucoz.com", "CENG", "Associate Professor");
 //		a1.giris();
 		
-		Memur m1 = new Memur("Charmine Bethune", "149163120247", "cbethune2@edublogs.org", "Bilgi Iþlem Müdür", "12:05:20");
-	//	m1.yemekhane();
+		Officer o1 = new Officer("Charmine Bethune", "149163120247", "cbethune2@edublogs.org", "Information Processing Manager", "12:05:20");
+	//	o1.cafeteria();
 		
-		OgretimGorevlisi o1 = new OgretimGorevlisi("Alida Guilleton", "24716766227", "aguilleton3@mozilla.com", "CENG", "PROF", "106");
-	//	o1.giris();
-//		o1.derseGir();
-	//	System.out.println(o1.getKapiNo());
+		Instructor i1 = new Instructor("Alida Guilleton", "24716766227", "aguilleton3@mozilla.com", "CENG", "PROF", "106");
+	//	i1.giris();
+//		i1.derseGir();
+	//	System.out.println(i1.getKapiNo());
 		
-		Asistan as1 = new Asistan("Cherin Syddall", "19083241143", "csyddall4@usgs.gov", "CENG", "DR", "102");        
-//      as1.quizYap();
+		Assistant as1 = new Assistant("Cherin Syddall", "19083241143", "csyddall4@usgs.gov", "CENG", "DR", "102");        
+//      	as1.quizYap();
         
-        LabAsistan la1 = new LabAsistan("Skyler Kirke", "39167223144", "skirke9@goo.ne.jp", ".CENG", "YL", "201");
-//      la1.lablaraGir();
+		LabAssistant la1 = new LabAssistant("Skyler Kirke", "39167223144", "skirke9@goo.ne.jp", ".CENG", "YL", "201");
+//      	la1.lablaraGir();
         
-        BilgiIslem bi1 = new BilgiIslem("Gerty Schulter", "23357221107", "gschulter8@csmonitor.com", "Bilgi Iþlem", "16:05:20", "Bilgi Iþlem Sekreter");
-//      bi1.networkKurulumu();
-        
-        GuvenlikGorevlisi gg1 = new GuvenlikGorevlisi("Junina Parkins", "63202176120", "jparkins5@creativecommons.org", "Güvenlik Departman", "20:05:20", "Güvenlik Görevlisi Müdür");       
-//      gg1.nobet();
+        	InfoProcessing ip1 = new InfoProcessing("Gerty Schulter", "23357221107", "gschulter8@csmonitor.com", "Information Processing", "16:05:20", "Information Processing Secretary");
+	//      ip1.networkInstallation();
+
+		SecurityGuard sg1 = new SecurityGuard("Junina Parkins", "63202176120", "jparkins5@creativecommons.org", "Security Departmant", "20:05:20", "Security Guard Manager");       
+	//      sg1.nobet();
         
 
 	}
